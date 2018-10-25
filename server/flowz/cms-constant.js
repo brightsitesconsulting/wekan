4c0e9f87447a56c33f947b2da46b0dd00a6b1276
Tom Marulak <sitorush@gmail.com>
2018-10-25 16:46:45 +0100

    fixed error double export

--- server/flowz/cms-constant.js
+++ server/flowz/cms-constant.js
@@ -1,15 +1,17 @@
-export const NODE_TYPE = 'simple_article';
+let api_base = 'https://flowz-cms.herokuapp.com';
+let grant_type = 'client_credentials';
+let client_id = '3d44ec87-1865-4d9e-b8c4-7111367c2376';
+let client_secret = 'INES12northcliffe';
 
 if (/localhost/.test(process.env.ROOT_URL)) {
-  export const API_BASE = 'http://docker.for.mac.localhost:8082/';
-  export const GRANT_TYPE = 'client_credentials';
-  export const CLIENT_ID = '1f6bcc78-5a86-49ed-a395-767b756e8a7f';
-  export const CLIENT_SECRET = 'coba1234';
-} else {
-  export const API_BASE = 'https://flowz-cms.herokuapp.com';
-  export const GRANT_TYPE = 'client_credentials';
-  export const CLIENT_ID = '3d44ec87-1865-4d9e-b8c4-7111367c2376';
-  export const CLIENT_SECRET = 'INES12northcliffe';
+  api_base = 'http://docker.for.mac.localhost:8082/';
+  grant_type = 'client_credentials';
+  client_id = '1f6bcc78-5a86-49ed-a395-767b756e8a7f';
+  client_secret = 'coba1234';
 }
 
-
+export const NODE_TYPE = 'simple_article';
+export let API_BASE = api_base;
+export let GRANT_TYPE = grant_type;
+export let CLIENT_ID = client_id;
+export let CLIENT_SECRET = client_secret;
